ee88ed65093834c80cda857369a5c398f59c76b4
deathvu <romansteel22@gmail.com>
2021-01-26 04:23:39 +0300

    errors & solver finished at, testing func

--- src/solution.py
+++ src/solution.py
@@ -1,50 +1,68 @@
+import re
 import os
 from os.path import join, getsize
 
-'''
-for root, dirs, files in os.walk('../logs'):
-    print("root: ", root)
-    print("dirs: ", dirs)
-    print("files: ", files)
-    print('\n')
-    '''
-
 def traversal(path, level=1):
     print("level = ", level, "files: ", os.listdir(path))
-    if level == 3:  
-        if [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))] != ['ft_reference', 'ft_run']: # check if are the ft_reference and ft_run in test directories 
-            with open(path+"/"+"report.txt", "a") as rep:
+    if level == 3:
+        with open(path+"/"+"report.txt", "a") as rep:
+            # check if are the ft_reference and ft_run in test directories 
+            if [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))] != ['ft_reference', 'ft_run']:
                 rep.write('directory missing: '+' '.join([x for x in ['ft_reference','ft_run'] if x not in os.listdir(path)])+'\n')
                 return 0
-
-        else:
-            if os.listdir(path+'/ft_reference') != os.listdir(path+'/ft_run'):
-               # if len(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))) != 0:
-               ref_diff_list = list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))
-               run_diff_list = list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path+'/ft_reference'))) 
-               separator = ", "
+            # missing and extra files checking in ft_run and ft_reference
+            else:
+                if os.listdir(path+'/ft_reference') != os.listdir(path+'/ft_run'):
+                    
+                # save difference between files in catalogues in two lists (ft_reference - ft_run) and
+                # (ft_run - ft_reference)
+                    
+                    ref_diff_list = list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))
+                    run_diff_list = list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path+'/ft_reference'))) 
+                    separator = ", "
                
-               if ref_diff_list != []:
-                   print("In ft_run there are missing files present in ft_reference:", separator.join(['\''+ x+os.path.join('/'+str(''.join(os.listdir(path+'/ft_reference/'+x))))+'\'' for x in ref_diff_list]))
+                    if ref_diff_list != []:
+                        with open(path+"/report.txt", "a") as rep:
+                            rep.write("In ft_run there are missing files present in ft_reference:" + separator.join(['\''+ x+os.path.join('/'+str(''.join(os.listdir(path+'/ft_reference/'+x))))+'\'' for x in ref_diff_list])+'\n')
 
-               if run_diff_list != []:
-                   print("In ft_run there are extra files not present in ft_reference:", separator.join(['\''+ x+os.path.join('/'+str(''.join(os.listdir(path+'/ft_run/'+x))))+'\'' for x in run_diff_list]))
+                    if run_diff_list != []:
+                        with open(path+"/report.txt", "a") as rep:
+                            rep.write("In ft_run there are extra files not present in ft_reference:" + separator.join(['\''+ x+os.path.join('/'+str(''.join(os.listdir(path+'/ft_run/'+x))))+'\'' for x in run_diff_list])+'\n')
+                
+                
+                    return 0
+        
+                else:
+                    # if ft_reference and ft_run are equal by number of tests start 
+                    # searching files with errors or without "Solver finished at"
+                    for root, dirs, files in os.walk(path+'/ft_run/'):
+                        if files != []:
+                            for x in files: # in case if in ft_run/*/ will be more than 1 file
+                                with open(root+'/'+ x, "r") as rfile:
+                                    flines = rfile.readlines()
+                                    for num, z in enumerate(flines, 1):
+                                        if re.search("error", z, flags=re.IGNORECASE):
+                                            with open(path+'/'+"report.txt", "a") as rep:
+                                                rep.write(root.split('/').pop()+'/'+x+'('+str(num)+'): '+z)
 
-               print('ref: ', ref_diff_list)
-               print('run:', run_diff_list)
-               print('\n\n\n\n\n\nFOUND\n\n\n\n')
+                                    if re.search(r'Solver finished at', ' '.join(flines)) == None:
+                                        with open(path+'/'+"report.txt", "a") as rep:
+                                           rep.write(root.split('/').pop()+'/'+x+": missing 'Solver finished at'")
 
+#                                    ft_run_mem_peak = 
 
-#                   print(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))
-#                   print(' '.join(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path + '/ft_run')))) + '/\n\n\n\n\n\nFOUND\n\n\n')
+                                           
+#                    ft_run_files = os.listdir(path + '/ft_run/' 
+#                    ft_references_files = os.listdir(path + '/ft_reference') 
+#                    print(ft_run_files,'\n', ft_references_files, '\n\n\n\n\n')
+#                    
+                            
 
-#               if list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path+'/ft_reference'))) != []:
-#                   print(' '.join(list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path + '/ft_reference')))) + '/\n\n\n\n\n\nFOUND\n\n\n')        
 
     for i in os.listdir(path):
         if os.path.isdir(path+'/'+i):
-            print("spuskaemsya", path+'/'+i)
+            print("spuskaemsya", path+i)
             traversal(path+'/'+i, level+1)
 #            print("vozvrat v", path)
 
-traversal('../logs/')
+traversal('../logs')
--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 import os
+import re
 #print([x for x in os.listdir('../logs/13-ROTATED_FLOWS') if os.path.isdir(x)])
 '''for i in os.listdir('../logs/'):
     if os.path.isdir(os.path.join('../logs/', i)):
@@ -9,14 +11,45 @@ with open("../logs/13-ROTATED_FLOWS/00060-RRF-u_R_0_IW/report.txt", "a") as rep:
     rep.write('directory missing: '+' '.join([x for x in ['ft_reference','ft_run'] if x not in os.listdir('../logs/13-ROTATED_FLOWS/00060-RRF-u_R_0_IW/')]))
     
 '''
-print(list(set(['1', '2', '3']) - set(['1', '2'])))
+#print(list(set(['1', '2', '3']) - set(['1', '2'])))
+#
+#        else:
+#            if os.listdir(path+'/ft_reference') != os.listdir(path+'/ft_run'):
+#               # if len(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))) != 0:
+#               if list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run'))) != []:
+#                   print(' '.join(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path + '/ft_run')))) + '/\n\n\n\n\n\nFOUND\n\n\n')
+#
+#               if list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path+'/ft_reference'))) != []:
+#                   print(' '.join(list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path + '/ft_reference')))) + '/\n\n\n\n\n\nFOUND\n\n\n')        
+#
 
-        else:
-            if os.listdir(path+'/ft_reference') != os.listdir(path+'/ft_run'):
-               # if len(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run')))) != 0:
-               if list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path+'/ft_run'))) != []:
-                   print(' '.join(list(set(os.listdir(path+'/ft_reference')) - set(os.listdir(path + '/ft_run')))) + '/\n\n\n\n\n\nFOUND\n\n\n')
+regex = re.compile("[-+]?\d*\.\d+|\d+") 
 
-               if list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path+'/ft_reference'))) != []:
-                   print(' '.join(list(set(os.listdir(path+'/ft_run')) - set(os.listdir(path + '/ft_reference')))) + '/\n\n\n\n\n\nFOUND\n\n\n')        
 
+def setPeak(ifile):
+    with open(ifile, "r") as rfile:
+        lines = rfile.readlines()
+        list_of_floats = []
+        for x in lines:
+            if re.search("Memory Working Set Peak", x, re.IGNORECASE):
+                list_of_floats.append(float(re.search(regex, x.split(',')[1]).group(0)))
+        print(list_of_floats)
+    #    for z in list_of_floats:
+        print('max: ', max(list_of_floats))
+        return max(list_of_floats)
+
+run = setPeak('../logs/13-ROTATED_FLOWS/00061-RRF-u_R_0_IW_ref_boundary_lev_2/ft_run/1/1.stdout')
+print('\n')
+reference = setPeak('../logs/13-ROTATED_FLOWS/00061-RRF-u_R_0_IW_ref_boundary_lev_2/ft_reference/1/1.stdout')
+
+print('compare max run on min ref: ', "%.2f" % (Decimal(run) / Decimal(reference)))
+#    found = re.findall(r'Memory Working Set Peak', ' '.join(lines))
+#    print('found line', found)
+#    regex = re.compile(r'([1-9]+)', re.I)
+#    print(regex.search(' '.join(lines)).groups())
+
+
+# os.walk(.../.../..//.//../ft_run/1/1.stdout)
+# if files != []:
+#       func(root), then comparsion
+#       func(root.replace(ft_run->ft_reference) 
